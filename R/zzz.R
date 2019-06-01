248aaa67ba5813ab74d82937110a0c2899838bf0
John (JP) Coene <jcoenep@gmail.com>
2019-06-01 16:41:07 +0200

    corrected pkg name and load msg

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
-.onAttach <- function(libname = find.package("oRion"), pkgname = "oRion") {
+.onAttach <- function(libname = find.package("newsapi"), pkgname = "newsapi") {
 
   key <- Sys.getenv("NEWSAPI_API_KEY")
 
   msg <- "No API key found, see `newsapi_key`"
-  if(nchar(key) > 1) msg <- "API key found!"
+  if(nchar(key) > 1) msg <- "API key loaded!"
 
   packageStartupMessage(msg)
 
